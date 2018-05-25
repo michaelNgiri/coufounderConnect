c271bd6f9c26dc2d5d873910fd36a790fedb6f89
alveis <michaelngiri@gmail.com>
2018-05-25 16:00:17 +0100

    mordification to profile view blade

---
+++ resources/views/connections/view-profile.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <em style="font-size: 1em; color: grey; text-transform:capitalize; float: left;"></em>
+                    <p style="float: right;  padding-right: 48px;">Profile View</p>
+                </div>
+
+                @auth
+                <div class="card-body" style="text-align: center;">
+                    <!-- @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif -->
+
+
+                    <div class="profileHeader alert alert-success">
+                        <img height="120px" width="100px" src="{{asset(auth()->user()->imagePath())}}">
+                       <!--  @if(!Auth::user()->isVerified())
+                        <hr>
+                        <p class="alert-text alert-danger">
+                            Your account is not yet verified, please check your email for the verification link
+                        </p>
+                        <button class="btn alert-danger">Resend Verification Link</button>
+                        @endif -->
+                        <hr>
+                        
+                        <div class="connectionLinks" style="display: inline;">
+                            <a style="color: #fff;" href="{{route('connections.connect')}}" class="btn btn-primary">
+                                <i class="mdi mdi-link"></i>
+                            {{ __('connect') }}
+                        </a>
+                            <a style="color: #fff;" href="{{route('messaging.sendMessage')}}" class="btn btn-primary">
+                                <i class="mdi mdi-message"></i>
+                            {{ __('Message') }}
+                        </a>
+                             <a style="color: #fff;" href="{{route('messaging.sendMessage')}}" class="btn btn-primary">
+                                <i class="mdi mdi-check-all"></i>
+                            <!--  {{ __('Follow  ') }} -->
+                         </a>
+                        </div>
+                        
+                    </div>
+                    <div class="profileDetails" style="text-align: left;"><hr>
+                    <p><b>Name:</b> {{$user->name()}}</p>
+                    <p><b>Address: {{$user->address}} </b></p>
+                    <p><b>Age:</b></p>
+                    <p><b>City:</b></p>
+                    <p><b>Country of Residence:</b></p>
+                    <p><b>AVailability:</b></p>
+                    </div>
+                </div>
+
+                @else
+                <div class="card-body" style="text-align: center;">
+                    please login to view your profile
+                </div>
+                @endauth
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
