00823bdeb047f3dcd7140c87322feb9ea219e250
alveis <michaelngiri@gmail.com>
2018-05-25 15:59:38 +0100

    preemptive action to catch every form of error

---
+++ resources/views/errors/error.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="row"><br><br>
+	<div class="col-md-12" style="text-align: center; color: #d3d3d3; position: fixed;">
+		<h3>Oops Something went wrong</h3>
+		<i style="font-size: 24px; text-align: center;" class="mdi mdi-emoticon-sad"></i>
+		<h5>Please use the navigation bar to return to the previous page</h5>
+	</div>
+</div>
+@endsection
