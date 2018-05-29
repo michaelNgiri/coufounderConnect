c1cac6974fc6d37e79394b927facc5131a713740
alveis <michaelngiri@gmail.com>
2018-05-29 23:03:04 +0100

    basics for discussions

---
+++ resources/views/discussions/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <a href="" class="btn btn-secondary">Start a Discussion Thread +</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+
+            </div>
+        </div>
+    </div>
+@endsection
