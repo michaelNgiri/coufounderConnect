901aa315264d8ccd0c612c08ff6e07757c369a69
alveis <michaelngiri@gmail.com>
2018-06-08 13:23:57 +0100

    confirm join view

---
+++ resources/views/mailing/confirm-join.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 center-align">
+            <i style="font-size: 10em; text-align: center;" class="mdi mdi-message white teal-text"></i>
+            <p class="teal-text">You will now receive periodic mails from us</p>
+        </div>
+    </div>
+</div>
+@endsection
