189fb8b0c8e735a2c5fb02dd1ce4fc11d22bb48d
alveis <michaelngiri@gmail.com>
2018-06-08 13:21:23 +0100

    new updates to messages migration

--- database/migrations/2018_05_25_153624_create_messages_table.php
+++ database/migrations/2018_05_25_153624_create_messages_table.php
@@ -20,19 +20,18 @@ class CreateMessagesTable extends Migration
             $table->text('title');
             $table->longText('message_body');
             $table->boolean('has_attachment')->default(false);
-            $table->boolean('sent')->default(false);
             $table->boolean('received')->default(false);
-            $table->boolean('read')->default(false);
+            $table->timestamp('read_at')->nullable();
 //            $table->boolean('scheduled')->default(false);
 //            $table->timestamp('schedule_time')->default(\Carbon\Carbon::now());
 
-            $table->foreign('sender_id', 'sender_user_id')
+            $table->foreign('sender_id', 'message_sender_user_id')
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
 
-            $table->foreign('recipient_id', 'recipient_user_id')
+            $table->foreign('recipient_id', 'message_recipient_user_id')
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade')
