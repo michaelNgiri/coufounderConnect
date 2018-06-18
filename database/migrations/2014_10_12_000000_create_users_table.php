3948df6b432c0ec74bb46a66eb86beb73c12ab6b
alveis <michaelngiri@gmail.com>
2018-06-18 20:06:16 +0100

    make firstname, lastname and username nullable, add unique slu go users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('username')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->string('slug')->unique();
+            $table->string('slug', 190)->unique();
             $table->boolean('is_admin')->default(false);
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
